821fefd2647e74e99e9be9a79b039e2e2a4be6c8
lucasyarid <lucas.yarid@gmail.com>
2017-07-11 23:26:23 +1200

    add order and featured custom fields

--- archive-rooms.php
+++ archive-rooms.php
@@ -28,74 +28,87 @@ $pagename = 'rooms';
 </div>
 
 <div id="rooms" <?php Avada()->layout->add_style( 'content_style' ); ?>>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<?php echo wp_kses_post( fusion_render_rich_snippets_for_pages() ); ?>
+	<?php // query
+	$the_query = new WP_Query(array(
+		'post_status' 		=> 'publish',
+		'post_type'			=> 'rooms',
+		'posts_per_page'	=> -1,
+		'meta_key' 			=> 'order',
+        'orderby' 			=> 'meta_value_num',
+        'order' 			=> 'ASC'
+	));
 
-			<div class="post-content rooms-list">
+	?>
+	<?php if( $the_query->have_posts() ): ?>
+		<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php echo wp_kses_post( fusion_render_rich_snippets_for_pages() ); ?>
 
-	        	<div class="fusion-builder-row fusion-row ">
-	        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_2  fusion-one-half fusion-column-first 1_2" style="width:50%;width:calc(50% - ( ( 1% + 1% ) * 0.5 ) );margin-right: 1%;">
-	        			<div class="fusion-column-wrapper">
-	        				<?php include('partials/ACFfusionSlider.php'); ?>
-	        				<div class="fusion-clearfix"></div>
-	        			</div>
-	        		</div>
-	        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_4  fusion-one-fourth 1_4" style="width:25%;width:calc(25% - ( ( 1% + 1% ) * 0.25 ) );margin-right: 1%;">
-	        			<div class="fusion-column-wrapper">
-							<div class="room-content">
-								<div class="room-title">
-									<h2><?php the_title(); ?></h2>
+				<div class="post-content rooms-list">
+
+		        	<div class="fusion-builder-row fusion-row ">
+		        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_2  fusion-one-half fusion-column-first 1_2" style="width:50%;width:calc(50% - ( ( 1% + 1% ) * 0.5 ) );margin-right: 1%;">
+		        			<div class="fusion-column-wrapper">
+		        				<?php include('partials/ACFfusionSlider.php'); ?>
+		        				<div class="fusion-clearfix"></div>
+		        			</div>
+		        		</div>
+		        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_4  fusion-one-fourth 1_4" style="width:25%;width:calc(25% - ( ( 1% + 1% ) * 0.25 ) );margin-right: 1%;">
+		        			<div class="fusion-column-wrapper">
+								<div class="room-content">
+									<div class="room-title">
+										<h2><?php the_title(); ?></h2>
+									</div>
+									<div class="room-description">
+										<?php the_content(); ?>
+									</div>
 								</div>
-								<div class="room-description">
-									<?php the_content(); ?>
-								</div>
-							</div>
-	        				<div class="fusion-clearfix"></div>
-	        			</div>
-	        		</div>
-	        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_4  fusion-one-fourth fusion-column-last 1_4" style="width:25%;width:calc(25% - ( ( 1% + 1% ) * 0.25 ) );">
-	        			<div class="fusion-column-wrapper">
-	        				<div class="room-pricing">
-	        					<ul>
-	        						<?php if( get_field('pricing_one_person') ): ?>
-	        							<li>$<?php the_field('pricing_one_person'); ?> – 1 person</li>
-	        						<?php endif; ?>
-	        						<?php if( get_field('pricing_two_people') ): ?>
-	        							<li>$<?php the_field('pricing_two_people'); ?> – 2 people</li>
-	        						<?php endif; ?>
-	        						<?php if( get_field('pricing_three_people') ): ?>
-	        							<li>$<?php the_field('pricing_three_people'); ?> – 3 people</li>
-	        						<?php endif; ?>
-	        					</ul>
-	        				</div>
-	        				<div class="fusion-clearfix"></div>
-	        			</div>
-	        		</div>
-	        	</div>
-
-				<?php if( get_field('dimensions') ): ?>
-		        	<div class="room-dimensions dimensions">
-		        		<?php the_field('dimensions'); ?>
+		        				<div class="fusion-clearfix"></div>
+		        			</div>
+		        		</div>
+		        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_4  fusion-one-fourth fusion-column-last 1_4" style="width:25%;width:calc(25% - ( ( 1% + 1% ) * 0.25 ) );">
+		        			<div class="fusion-column-wrapper">
+		        				<div class="room-pricing">
+		        					<ul>
+		        						<?php if( get_field('pricing_one_person') ): ?>
+		        							<li>$<?php the_field('pricing_one_person'); ?> – 1 person</li>
+		        						<?php endif; ?>
+		        						<?php if( get_field('pricing_two_people') ): ?>
+		        							<li>$<?php the_field('pricing_two_people'); ?> – 2 people</li>
+		        						<?php endif; ?>
+		        						<?php if( get_field('pricing_three_people') ): ?>
+		        							<li>$<?php the_field('pricing_three_people'); ?> – 3 people</li>
+		        						<?php endif; ?>
+		        					</ul>
+		        				</div>
+		        				<div class="fusion-clearfix"></div>
+		        			</div>
+		        		</div>
 		        	</div>
-		        <?php endif; ?>
 
-	        	<div class="room-link">
-	        		<?php if( get_field('button_link') ): ?>
-	        			<a class="button default-button" target="_blank" href="<?php the_field('button_link'); ?>">
-	        			    Book Here
-	        			</a>
-	        		<?php else : ?>
-	        			<a class="button default-button" target="_blank" href="https://secure.staah.com/common-cgi/package/packagebooking.pl?propertyId=4043&Ln=en">
-	        			    Book Here
-	        			</a>
-	        		<?php endif; ?>
-	        	</div>
+					<?php if( get_field('dimensions') ): ?>
+			        	<div class="room-dimensions dimensions">
+			        		<?php the_field('dimensions'); ?>
+			        	</div>
+			        <?php endif; ?>
 
-				<?php fusion_link_pages(); ?>
+		        	<div class="room-link">
+		        		<?php if( get_field('button_link') ): ?>
+		        			<a class="button default-button" target="_blank" href="<?php the_field('button_link'); ?>">
+		        			    Book Here
+		        			</a>
+		        		<?php else : ?>
+		        			<a class="button default-button" target="_blank" href="https://secure.staah.com/common-cgi/package/packagebooking.pl?propertyId=4043&Ln=en">
+		        			    Book Here
+		        			</a>
+		        		<?php endif; ?>
+		        	</div>
+
+					<?php fusion_link_pages(); ?>
+				</div>
 			</div>
-		</div>
-	<?php endwhile; ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
 </div>
 	<?php wp_reset_postdata(); ?>
 </div>
--- venues.php
+++ venues.php
@@ -14,8 +14,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php echo wp_kses_post( fusion_render_rich_snippets_for_pages() ); ?>
 	<?php
 		$args = array(
-			'post_type' => 'tribe_venue',
-			'posts_per_page' => -1
+			'post_status' 		=> 'publish',
+			'post_type' 		=> 'tribe_venue',
+			'posts_per_page'	=> -1,
+			'meta_key' 			=> 'order',
+	        'orderby' 			=> 'meta_value_num',
+	        'order' 			=> 'ASC'
 		);
 		// The Query
 		$the_query = new WP_Query( $args );
@@ -25,64 +29,67 @@ if ( ! defined( 'ABSPATH' ) ) {
 				$the_query->the_post(); ?>
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<?php echo wp_kses_post( fusion_render_rich_snippets_for_pages() ); ?>
+					<?php if( get_field('featured') ): ?>
+						<div class="post-content venues">
 
-					<div class="post-content venues">
-
-			        	<div class="fusion-builder-row fusion-row ">
-			        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_2  fusion-one-half fusion-column-first 1_2" style="width:50%;width:calc(50% - ( ( 1% + 1% ) * 0.5 ) );margin-right: 1%;">
-			        			<div class="fusion-column-wrapper">
-			        				<?php include('partials/ACFfusionSlider.php'); ?>
-			        				<div class="fusion-clearfix"></div>
-			        			</div>
-			        		</div>
-			        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_4  fusion-one-fourth 1_4" style="width:25%;width:calc(25% - ( ( 1% + 1% ) * 0.25 ) );margin-right: 1%;">
-			        			<div class="fusion-column-wrapper">
-									<div class="venue-content">
-										<div class="venue-title">
-											<h2><?php echo get_the_title(); ?></h2>
+				        	<div class="fusion-builder-row fusion-row ">
+				        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_2  fusion-one-half fusion-column-first 1_2" style="width:50%;width:calc(50% - ( ( 1% + 1% ) * 0.5 ) );margin-right: 1%;">
+				        			<div class="fusion-column-wrapper">
+				        				<?php include('partials/ACFfusionSlider.php'); ?>
+				        				<div class="fusion-clearfix"></div>
+				        			</div>
+				        		</div>
+				        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_4  fusion-one-fourth 1_4" style="width:25%;width:calc(25% - ( ( 1% + 1% ) * 0.25 ) );margin-right: 1%;">
+				        			<div class="fusion-column-wrapper">
+										<div class="venue-content">
+											<div class="venue-title">
+												<h2><?php echo get_the_title(); ?></h2>
+											</div>
+											<div class="venue-description">
+												<?php the_content(); ?>
+											</div>
 										</div>
-										<div class="venue-description">
-											<?php the_content(); ?>
-										</div>
-									</div>
-									<div class="venue-dimensions">
-										<?php the_field('dimensions'); ?>
-									</div>
-			        				<div class="fusion-clearfix"></div>
-			        			</div>
-			        		</div>
-			        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_4  fusion-one-fourth fusion-column-last 1_4" style="width:25%;width:calc(25% - ( ( 1% + 1% ) * 0.25 ) );">
-			        			<div class="fusion-column-wrapper">
-			        				<div class="venue-pricing">
-			        					<h3>Week Day & Weekends</h3>
-			        					<ul>
-			        						<?php if( get_field('pricing_week_day') ): ?>
-			        							<li><?php the_field('pricing_week_day'); ?> – Week day</li>
-			        						<?php endif; ?>
-			        						<?php if( get_field('pricing_weekend_day') ): ?>
-			        							<li><?php the_field('pricing_weekend_day'); ?> – Weekend day</li>
-			        						<?php endif; ?>
-			        						<?php if( get_field('pricing_weekend_day_sat_sun') ): ?>
-			        							<li><?php the_field('pricing_weekend_day_sat_sun'); ?> – Weekend Sat & Sun</li>
-			        						<?php endif; ?>
-			        					</ul>
-			        					<h3>Per Hours</h3>
-			        					<ul>
-			        						<?php if( get_field('pricing_week_day_hour') ): ?>
-			        							<li><?php the_field('pricing_week_day_hour'); ?> – per hour weekdays</li>
-			        						<?php endif; ?>
-			        						<?php if( get_field('pricing_weekend_day_hour') ): ?>
-			        							<li><?php the_field('pricing_weekend_day_hour'); ?> – per hour weekends</li>
-			        						<?php endif; ?>
-			        					</ul>
-			        				</div>
-			        				<div class="fusion-clearfix"></div>
-			        			</div>
-			        		</div>
-			        	</div>
+										<?php if( get_field('dimensions') ): ?>
+											<div class="venue-dimensions">
+												<?php the_field('dimensions'); ?>
+											</div>
+										<?php endif; ?>
+				        				<div class="fusion-clearfix"></div>
+				        			</div>
+				        		</div>
+				        		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_4  fusion-one-fourth fusion-column-last 1_4" style="width:25%;width:calc(25% - ( ( 1% + 1% ) * 0.25 ) );">
+				        			<div class="fusion-column-wrapper">
+				        				<div class="venue-pricing">
+				        					<h3>Week Day & Weekends</h3>
+				        					<ul>
+				        						<?php if( get_field('pricing_week_day') ): ?>
+				        							<li><?php the_field('pricing_week_day'); ?> – Week day</li>
+				        						<?php endif; ?>
+				        						<?php if( get_field('pricing_weekend_day') ): ?>
+				        							<li><?php the_field('pricing_weekend_day'); ?> – Weekend day</li>
+				        						<?php endif; ?>
+				        						<?php if( get_field('pricing_weekend_day_sat_sun') ): ?>
+				        							<li><?php the_field('pricing_weekend_day_sat_sun'); ?> – Weekend Sat & Sun</li>
+				        						<?php endif; ?>
+				        					</ul>
+				        					<h3>Per Hours</h3>
+				        					<ul>
+				        						<?php if( get_field('pricing_week_day_hour') ): ?>
+				        							<li><?php the_field('pricing_week_day_hour'); ?> – per hour weekdays</li>
+				        						<?php endif; ?>
+				        						<?php if( get_field('pricing_weekend_day_hour') ): ?>
+				        							<li><?php the_field('pricing_weekend_day_hour'); ?> – per hour weekends</li>
+				        						<?php endif; ?>
+				        					</ul>
+				        				</div>
+				        				<div class="fusion-clearfix"></div>
+				        			</div>
+				        		</div>
+				        	</div>
 
-						<?php fusion_link_pages(); ?>
-					</div>
+							<?php fusion_link_pages(); ?>
+						</div>
+					<?php endif; ?>
 				</div>
 			<?php }
 			/* Restore original Post Data */
